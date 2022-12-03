21394270ceea722469f16c67b96c6cfbffbb24f6
Angeliki Papaioannou <angeliki.papaioannou@channelvas.com>
2022-12-03 22:15:07 +0200

    misc: added support for flags on scripts

--- misc/secureCopy.sh
+++ misc/secureCopy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+## Utility script to secure copy files from local to remote env via ssh
+## You can update the parameters inline or use flags
+## Example with flags: ./secureCopy.sh -f /home/angel -t /temp/files -u angel -e happy.prod
+## To execute without flags just update inline the parameters and run
+
 #some colors
 CYAN="\e[96m"
 GREEN="\e[92m"
@@ -13,8 +18,31 @@ toDir=/tmp
 username=angel
 environment=happy.prod
 
-echo -e "${CYAN} Start copying from: " $fromDir "${ENDCOLOR}"
-echo -e "${GREEN} on evn: " $environment "${ENDCOLOR}"
-echo -e "${MAGENTA} under: " $toDir "${ENDCOLOR}"
+##Use flags if you prefer
+
+while getopts 'f:t:u:e:' OPTION; do
+  case "$OPTION" in
+    f)
+      fromDir="$OPTARG"
+      ;;
+    t)
+      toDir="$OPTARG"
+      ;;
+    u)
+      username="$OPTARG"
+      ;;
+    e)
+      environment="$OPTARG"
+      ;;      
+    ?)
+      echo "script usage: secureCopy.sh [-f somevalue] [-t somevalue] [-u somevalue] [-e somevalue]"
+      exit 1
+      ;;
+  esac
+done
+
+echo -e "${CYAN} Start copying from: $fromDir ${ENDCOLOR}"
+echo -e "${GREEN} on evn: $username@$environment ${ENDCOLOR}"
+echo -e "${MAGENTA} under: $toDir ${ENDCOLOR}"
 
 scp -r $fromDir $username@$environment:$toDir
--- misc/sshTunnel.sh
+++ misc/sshTunnel.sh
@@ -4,6 +4,10 @@
     #SSH tunneling, or SSH port forwarding, is a method of transporting arbitrary data over an encrypted SSH connection. 
     #SSH tunnels allow connections made to a local port (that is, to a port on your own desktop) to be forwarded to a remote machine via a secure channel.
 
+## How to execute:
+    # Example with flags: ./sshTunnel.sh -p [local_port] -r [remote:port]  -u [username] -h [remote_host]
+    # To execute without flags just update inline the parameters and run    
+
 #some colors
 CYAN="\e[96m"
 GREEN="\e[92m"
@@ -12,13 +16,36 @@ ENDCOLOR="\e[0m"
 
 
 ##Parameters: Update here :)
-ssh_host=happy.remote.server
-username=angel
 local_port=8888
 host_port=8888
+ssh_host=happy.remote.server
+username=angel
 
-echo -e "${CYAN} Starting tunnel from local port : " $local_port "${ENDCOLOR}"
-echo -e "${GREEN} to host port: " $host_port "${ENDCOLOR}"
-echo -e "${MAGENTA} on host: " $ssh_host "${ENDCOLOR}"
+##Use flags if you prefer
+
+while getopts 'p:r:u:h:' OPTION; do
+  case "$OPTION" in
+    p)
+      local_port="$OPTARG"
+      ;;
+    r)
+      host_port="$OPTARG"
+      ;;
+    u)
+      username="$OPTARG"
+      ;;
+    h)
+      ssh_host="$OPTARG"
+      ;;      
+    ?)
+      echo "script usage: "
+      exit 1
+      ;;
+  esac
+done
+
+echo -e "${CYAN} Starting tunnel from local port : $local_port ${ENDCOLOR}"
+echo -e "${GREEN} to host port:  $host_port ${ENDCOLOR}"
+echo -e "${MAGENTA} on host:  $username@$ssh_host ${ENDCOLOR}"
 
 ssh -L $local_port:$ssh_host:$host_port $username@$ssh_host
