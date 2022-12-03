701ae55e2c3ada4fabdd2a18faf7cb2e52135e68
Angeliki Papaioannou <angelikipap@users.noreply.github.com>
2022-12-03 22:33:58 +0200

    git: pullProjects.sh added some colors

--- git/pullProjects.sh
+++ git/pullProjects.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Place this script under a directory with git repos and execute it in order to update all repos in the directory
-# Credits to Github Alexandros Mertzianis (GitHub Account:alexmertzianis)
+# Credits to Github Alexandros Mertzianis (GitHub Account:alexmertzianis) for the initial script
+
+#some colors
+RED="\e[91m"
+GREEN="\e[92m"
+ENDCOLOR="\e[0m"
+
 
 projects=$(ls)
 
@@ -11,7 +17,7 @@ for project in ${projects[@]};do
     git rev-parse --abbrev-ref HEAD
     echo "Pulling latest........ "
     git pull
-    [[ $? == 0 ]] && echo "OK" || echo "ERROR"
+    [[ $? == 0 ]] && echo -e "${GREEN} OK $fromDir ${ENDCOLOR}" || echo -e "${RED} ERROR: $fromDir ${ENDCOLOR}"
     cd ..
     echo "-------------------------------------------------------------------------------------------------"
 done
